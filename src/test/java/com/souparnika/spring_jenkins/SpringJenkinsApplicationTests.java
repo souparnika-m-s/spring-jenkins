c164a062e0f34aac36af7e6bc1bd5331546dcd46
souparnikams <souparnika.ms@flytxt.com>
2024-09-08 22:47:07 +0530

    second commit

--- src/test/java/com/souparnika/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/souparnika/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
@@ -14,7 +14,8 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
-		assertEquals(true, true);
+		logger.info("Just changes the assert statement...");
+        assertTrue(true);
 	}
 
 }
