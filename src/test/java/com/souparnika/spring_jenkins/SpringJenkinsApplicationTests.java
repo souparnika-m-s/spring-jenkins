dc8cf0b34a2fabb5eaa6ca83aa04a2f3199e7ae1
souparnikams <souparnika.ms@flytxt.com>
2024-10-12 17:13:01 +0530

    removed void

--- src/test/java/com/souparnika/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/souparnika/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ class SpringJenkinsApplicationTests {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		logger.info("Test case executing...");
         assertTrue(true);
 	}
